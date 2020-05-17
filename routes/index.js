9f66b54c17679a3e17243e81bbc27422490b933b
Alex Shortt <hello@alex-shortt.com>
2020-05-17 10:10:58 +0100

    :art: Add test product ID base 64

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,60 @@
-const ContentfulClient = require('../_clients/Contentful')
-const ShopifyClient = require('../_clients/Shopify')
-const Product = require('../_models/Product')
-const ProductDecorator = require('../_models/ProductDecorator')
-const { Router } = require('express')
+const ContentfulClient = require("../_clients/Contentful");
+const ShopifyClient = require("../_clients/Shopify");
+const Product = require("../_models/Product");
+const ProductDecorator = require("../_models/ProductDecorator");
+const { Router } = require("express");
 
-const router = Router()
+const router = Router();
 
-router.get('/collections', function(req, res, next) {
+router.get("/collections", function (req, res, next) {
   ShopifyClient.collection.fetchAllWithProducts().then((collections) => {
-    res.json(collections)
-  })
-})
+    res.json(collections);
+  });
+});
 
-router.get('/products', function(req, res, next) {
+router.get("/products", function (req, res, next) {
   ShopifyClient.product.fetchAllWithTags().then((data) => {
     const products = data.map((productData) => {
-      const product = new Product(productData)
-      return product.get
-    })
-    res.json(products)
-  })
-})
+      const product = new Product(productData);
+      return product.get;
+    });
+    res.json(products);
+  });
+});
 
-router.get('/products/:query', function(req, res, next) {
-  const { query } = req.params
-
-  const productFetcher = (query.match(/^[0-9]+$/) != null)
+router.get("/products/:query", function (req, res, next) {
+  let { query } = req.params;
+  const isIdQuery = query.match(/^[0-9]+$/) != null;
+  const productFetcher = isIdQuery
     ? ShopifyClient.product.fetch(query)
-    : ShopifyClient.product.fetchByHandle(query)
+    : ShopifyClient.product.fetchByHandle(query);
+
+  if (isIdQuery) {
+    query = 'NDYxMzA2Njk4MTUwOQ=='
+  }
 
   productFetcher
     .then((data) => {
-      const product = new Product(data)
-      const id = product.getId
+      const product = new Product(data);
+      const id = product.getId;
       ContentfulClient.getEntries({
-        content_type: 'product',
-        'fields.id[in]': id
+        content_type: "product",
+        "fields.id[in]": id,
       }).then((entry) => {
         if (entry.total) {
           const decoratedProduct = new ProductDecorator(
             product.get,
             entry.items[0].fields
-          )
-          res.json(decoratedProduct.get)
+          );
+          res.json(decoratedProduct.get);
         } else {
-          res.json(product.get)
+          res.json(product.get);
         }
-      })
+      });
     })
     .catch(() => {
-      res.status(404).send('Product not found')
-    })
-})
+      res.status(404).send("Product not found");
+    });
+});
 
-module.exports = router
+module.exports = router;
